7220c1225c54ae9a33b87792b09e861d0713f046
omujsk <mujowoww@hotmail.com>
2022-11-22 23:39:25 +0100

    Task 222: Create Order Category entity

---
+++ src/Domain/Entities/OrderCategory.cs
@@ -0,0 +1,7 @@
+﻿namespace WiseConsulting.Domain.Entities;
+
+public class OrderCategory : BaseAuditableEntity
+{
+    public string Name { get; set; } = string.Empty;
+    public int Price { get; set; }
+}
