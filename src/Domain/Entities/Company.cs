a858d5b70f100e0dd509ddf586978deb8ed9f144
Jonuz Miftari <jonuz.miftari@wsa.com>
2022-12-06 22:16:27 +0100

    Task 223:Create Company entity

---
+++ src/Domain/Entities/Company.cs
@@ -0,0 +1,28 @@
+﻿namespace WiseConsulting.Domain.Entities;
+public class Company : BaseEntity
+{
+    public int Code { get; set; }
+    public string Name { get; set; }
+    public string? Address { get; set; }
+    public string? City { get; set; }
+    public string? Phone { get; set; }
+    public string? Fax { get; set; }
+    // TODO: change bank to bank entity
+    public string Bank { get; set; }
+    public string AccountNumber { get; set; }
+    // Maticen broj
+    public string? IDNumber { get; set; }
+    // Registarski broj
+    public string? RegistrationNumber { get; set; }
+    public string? TaxNumber { get; set; }
+    public string? ActivityCode { get; set; }
+    public int MunicipalityCode { get; set; }
+    public string? SecondMunicipalityCode { get; set; }
+    public string? SalaryCode { get; set; }
+    public int MunicipalityCodeForOtherCity { get; set; }
+    public string? ContactPerson { get; set; }
+    public string? Email { get; set; }
+    public string? InvoiceSigningPerson { get; set; }
+    public SigningPersonType InvoiceSigningPersonType { get; set; }
+    // TODO: add InvoiceSigningImage
+}
