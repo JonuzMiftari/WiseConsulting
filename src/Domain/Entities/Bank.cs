a58bba2dea9b117356a13a5bd85abef82478cfb7
omujsk <mujowoww@hotmail.com>
2022-12-09 15:38:26 +0100

    Task 226: Create Bank entity (fixed missing property fields)

--- src/Domain/Entities/Bank.cs
+++ src/Domain/Entities/Bank.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-namespace WiseConsulting.Domain.Entities
-{
-    public class Bank
-    {
-        public Bank()
-        {
-        }
-    }
-}
+namespace WiseConsulting.Domain.Entities;
 
+public class Bank : BaseAuditableEntity
+{
+    public string Name { get; set; } = string.Empty;
+    public string Identifier { get; set; } = string.Empty;
+}
