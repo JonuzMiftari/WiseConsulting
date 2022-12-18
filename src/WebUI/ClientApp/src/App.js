5a6d7eb877174fca789bdaae82e90a64c23500d3
Jonuz Miftari <jonuz.miftari@wsa.com>
2022-12-19 00:03:17 +0100

    Task 208

--- src/WebUI/ClientApp/src/App.js
+++ src/WebUI/ClientApp/src/App.js
@@ -10,15 +10,23 @@ export default class App extends Component {
   static displayName = App.name;
 
   render() {
-    return (
-      <Layout>
-        <Routes>
-          {AppRoutes.map((route, index) => {
-            const { element, requireAuth, ...rest } = route;
-            return <Route key={index} {...rest} element={requireAuth ? <AuthorizeRoute {...rest} element={element} /> : element} />;
-          })}
-        </Routes>
-      </Layout>
+      return (
+          <Dashbard>
+              {/*<Routes>*/}
+              {/*    {AppRoutes.map((route, index) => {*/}
+              {/*        const { element, requireAuth, ...rest } = route;*/}
+              {/*        return <Route key={index} {...rest} element={requireAuth ? <AuthorizeRoute {...rest} element={element} /> : element} />;*/}
+              {/*    })}*/}
+              {/*</Routes>*/}
+          </Dashbard>
+      //<Layout>
+      //  <Routes>
+      //    {AppRoutes.map((route, index) => {
+      //      const { element, requireAuth, ...rest } = route;
+      //      return <Route key={index} {...rest} element={requireAuth ? <AuthorizeRoute {...rest} element={element} /> : element} />;
+      //    })}
+      //  </Routes>
+      //</Layout>
     );
   }
 }
